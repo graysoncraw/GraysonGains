8e62c4df1c77cd9592e603ae8624ed7f19f9c311
graysoncraw <graysoncraw4d@gmail.com>
2025-11-03 09:51:31 -0600

    starting ts over

--- ng-ggains/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from "@angular/router";
-import { SigninComponent } from "./signin/signin.component";
-import { LandingComponent } from "./landing/landing.component";
-
-export const routes: Routes = [
-    { path: "", component: LandingComponent },
-    { path: "signin", component: SigninComponent },
-];
